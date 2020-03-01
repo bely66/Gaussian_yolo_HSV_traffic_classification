142361c04e54fa97d3e1ec332d207ba6366676d6
mohamed nabil <medo661998@gmail.com>
2020-03-01 18:02:36 +0200

    run script

---
+++ run.py
@@ -0,0 +1,18 @@
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu',dest = "gpu" ,type=int, default = -1,
+                    help='enter the number of gpu')
+
+parser.add_argument('--image_path',dest = "image" , default = "Images/Traffic.jpg",
+                    help='enter the path of the image')
+
+
+args = parser.parse_args()
+
+
+from Inference import Infer
+Inference_class = Infer(detect_thresh = 0.5,gpu=args.gpu)
+Inference_class.infer(args.image)
